4500689347dead9f33bec6289a4becfb8a856c58
Allan Uustal <allan.uustal@gmail.com>
2022-07-17 11:53:35 +0300

    [add] - exercises with ThymeLeaf

--- src/main/java/com/sda/javaee14/springdemojavaee14/homework/optional/OptionalExerciseMain.java
+++ src/main/java/com/sda/javaee14/springdemojavaee14/homework/optional/OptionalExerciseMain.java
@@ -1,47 +1,81 @@
 package com.sda.javaee14.springdemojavaee14.homework.optional;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public class OptionalExerciseMain {
-    public static void main(String[] args) {
 
-        int nickLenght = personNick() !=null ? personNick().length() :0;
-        System.out.println("Nick lenght is:" + nickLenght);
+    public static void main(String[] args) {
+        int nickLength = personNick() != null ? personNick().length() : 0;
+
+        System.out.println("nick length is: " + nickLength);
 
         Optional<String> maybeNick = personNickIfPresent();
-        // isPresent() method used
-        if (maybeNick.isPresent()){
+        if (maybeNick.isPresent()) {
             System.out.println(maybeNick.get());
         } else {
-            System.out.println("Sorry, no Nick");
+            System.out.println("Sorry, no nick");
         }
-
-        // isEmpty() method used
-        if (maybeNick.isEmpty()){
-            System.out.println("Sorry, no Nock");
+        if (maybeNick.isEmpty()) {
+            System.out.println("Sorry, no nick");
         } else {
-
             System.out.println(maybeNick.get());
         }
 
-        // orElse() method used
         System.out.println("Better usage of Optional");
-        System.out.println("nick:" + maybeNick.orElse("no Nick"));
+        System.out.println("nick: " + maybeNick.orElse("no nick"));
 
         System.out.println("Optional with lambda");
-        maybeNick.ifPresent();
+        maybeNick.ifPresent(new MyNickConsumer());
 
+        System.out.println("Now with my own lambda:)");
+        // lambda is about reducing ceremony
+        // lambda is for shorter way of providing interface implementations
+        // lambda is just content of missing interface method
+        // we don't need argument type in lambda because we know which method we implement
+        // lambda is for sending behaviour
+        // we can use lambda only with interfaces with just one abstract method (one to implement)
+        // -> (arrow) is separator of arguments and method body
+        maybeNick.ifPresent(nick -> System.out.println("Nick is: " + nick));
+        maybeNick.ifPresent((String nick) -> System.out.println("Nick is: " + nick));
+
+        System.out.println("Now more exercises with lambda");
+        Optional<String> hasContent = Optional.of("stupid content");
+        Optional<String> noContent = Optional.empty();
+
+        System.out.println("Using ifPresentOrElse method from Optional class with lambdas");
+        // first argument we implement accept method from Consumer
+        // second argument we implement run method from Runnable
+        hasContent.ifPresentOrElse(s -> System.out.println("Value is present: " + s),
+                () -> System.out.println("no content"));
+
+        noContent.ifPresentOrElse(s -> {
+                    System.out.println("has content");
+                    System.out.println("content: " + s);
+                },
+                () -> {
+                    System.out.println("no content");
+                    System.out.println("just null");
+                });
     }
 
-    public static String personNick(){
-
+    // In Kotlin Language: String (never null) and String?
+    public static String personNick() {
         return null;
     }
+
     // Optional is bag for just one item
     // we're using "factory methods" for creating instances of this class
     // factory method - is static method which is able to produce instances of classes and have good name
     public static Optional<String> personNickIfPresent() {
-        return Optional.empty();
+        return Optional.of("maniek");
     }
+}
 
+// generic interface Consumer - T will be replaced with String in our case
+class MyNickConsumer implements Consumer<String> {
+    @Override
+    public void accept(String t) {
+        System.out.println("Nick is: " + t);
+    }
 }
